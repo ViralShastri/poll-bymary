041e31942dbea48bde69d5c2fa17b3903a459f23
Hasan Mukati <mukati.hasan@gmail.com>
2020-09-14 23:17:01 -0400

    Changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,7 +8,8 @@ from home import views
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('/', admin.site.urls),
+    path('/', include('polls.urls')),
+    path('/favicon.ico', include('polls.urls')),
     path('', views.home_view, name='home'),
 
     
