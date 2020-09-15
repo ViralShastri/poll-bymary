6b1562805f42f6e74554d10a570f71104540b972
Hasan Mukati <mukati.hasan@gmail.com>
2020-09-15 16:22:12 -0400

    Changes

--- polls/migrations/0004_auto_20200913_2152.py
+++ polls/migrations/0004_auto_20200913_2152.py
@@ -31,7 +31,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             id = models.BigIntegerField(primary_key = True)
-            model_name='comment',
             name='id',
             field=models.AutoField(default=1, max_length=12, primary_key=True, serialize=False),
             preserve_default=False,
