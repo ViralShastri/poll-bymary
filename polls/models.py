830cd77fad37ad6950fad64ee75f0cc28d04185d
Hasan Mukati <mukati.hasan@gmail.com>
2020-09-15 15:30:11 -0400

    Changes

--- polls/models.py
+++ polls/models.py
@@ -42,7 +42,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
+class comment(models.Model):
+    id = models.BigIntegerField(primary_key = True)
     post = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
