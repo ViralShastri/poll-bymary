80379d400f49e0a58bfa1e248bd11dcc255e99cf
Hasan Mukati <mukati.hasan@gmail.com>
2020-09-15 20:30:58 -0400

    keys

--- polls/migrations/0002_comment.py
+++ polls/migrations/0002_comment.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID')),
                 ('author', models.CharField(max_length=200)),
                 ('text', models.TextField()),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
--- polls/migrations/0005_auto_20200913_2153.py
+++ polls/migrations/0005_auto_20200913_2153.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         
         migrations.AddField(
             model_name='comment',
-            name='id',
+            name='addid',
             field=models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
